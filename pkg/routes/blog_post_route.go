a79d7788d14409b10f231041a2ca6a28d2d11ed7
Arun Vishnu K <arunvishnuk008@gmail.com>
2025-11-15 22:59:27 +0530

    Implementation of get all blog posts API

--- pkg/routes/blog_post_route.go
+++ pkg/routes/blog_post_route.go
@@ -1,9 +1,35 @@
 package routes
 
-import "net/http"
+import (
+	"crimson-sunrise.site/pkg/common"
+	"crimson-sunrise.site/pkg/db"
+	"crimson-sunrise.site/pkg/persistence"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+)
 
 func GetAll(response http.ResponseWriter, request *http.Request) {
-
+	_ = request
+	allPosts, err := persistence.GetAllBlogPosts(db.DB)
+	if err != nil {
+		log.Printf("Failed to fetch blog posts with error %s", err.Error())
+		common.WriteJsonError(err,response, http.StatusNotFound)
+	}
+	if allPosts == nil || len(allPosts) == 0 {
+		log.Print("No posts found..")
+		common.WriteJsonError(errors.New("no blog posts found"),response, http.StatusNotFound)
+	}
+	jsonResponse, err := json.Marshal(allPosts)
+	if err != nil {
+		common.WriteJsonError(err,response, http.StatusInternalServerError)
+	}
+	response.Header().Add("Content-Type","application/json")
+	_, err = response.Write(jsonResponse)
+	if err != nil {
+		log.Printf("Failed to write blog posts json response with error %s", err.Error())
+	}
 }
 
 func NewPost(response http.ResponseWriter, request *http.Request) {
